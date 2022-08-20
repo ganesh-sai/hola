d7e5bea75c74dac429e54efcf870d5551bffec4b
ganesh-sai <avula.ganeshsai@gmail.com>
2022-08-20 15:07:35 +0530

    externalisze configuration proerties

--- src/main/java/com/gs/hola/HolaRestController.java
+++ src/main/java/com/gs/hola/HolaRestController.java
@@ -1,5 +1,6 @@
 package com.gs.hola;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,10 @@ import java.net.UnknownHostException;
 
 @RestController
 @RequestMapping("/api")
+@ConfigurationProperties(prefix = "helloapp")
 public class HolaRestController {
+    private String saying;
+
     @GetMapping(value = "/hola", produces = "text/plain")
     public String hola() throws UnknownHostException {
         String hostname = null;
@@ -18,6 +22,14 @@ public class HolaRestController {
         } catch (UnknownHostException e) {
             hostname = "unknown";
         }
-        return "Hola Spring Boot " + hostname;
+        return saying + " " + hostname;
+    }
+
+    public String getSaying() {
+        return saying;
+    }
+
+    public void setSaying(String saying) {
+        this.saying = saying;
     }
 }
